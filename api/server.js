991ff3d766fabda00c442d51988e78fbae408310
Muhammad Perdiansyah <muhammad.perdiansyah@inteleq.id>
2025-05-20 18:14:40 +0700

    refactor server.js

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 })
 router.get('/status', require('./status'));
 router.post('/send-message', express.json({ limit: '10mb' }), require('./send-message'));
