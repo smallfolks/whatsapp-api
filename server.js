35c73ac97583e0a0103e58ea3d383f61b94b615e
Muhammad Perdiansyah <muhammad.perdiansyah@inteleq.id>
2025-05-20 17:46:39 +0700

    Revert "update endpoint"
    This reverts commit d981d170

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// HTTP logging
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/status', require('./api/status'));
+app.post('/send-message', express.json({ limit: '10mb' }), require('./api/send-message'));
+app.post('/logout', require('./api/logout'));
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
